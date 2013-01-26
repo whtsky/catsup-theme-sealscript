04da98d913428e97145213ed214dad3e506a5ed9
whtsky <whtsky@me.com>
2013-01-26 23:53:31 +0800

    Update to the newest catsup.

--- theme.py
+++ theme.py
@@ -4,3 +4,25 @@ homepage = 'https://github.com/whtsky/catsup-theme-sealscript'
 pages = [
     '404.html'
 ]
+vars = {
+    "description": "a blog",
+    "twitter": "whouz",
+    "github": "whtsky",
+    "links": [
+        {
+            "name": "catsup",
+            "url": "https://github.com/whtsky/catsup",
+            "description": "Awesome!"
+        },
+        {
+            "name": "whtsky",
+            "url": "http://whouz.com",
+            "description": "I write catsup"
+        },
+        {
+            "name": "messense",
+            "url": "http://messense.me",
+            "description": "I also write catsup"
+        }
+    ]
+}
