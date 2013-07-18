6baa8e41a01277bf6e5cd5635c57471a7dd6ca49
whtsky <whtsky@me.com>
2013-07-18 14:42:50 +0800

    drop pages var

--- theme.py
+++ theme.py
@@ -1,9 +1,6 @@
 name = 'sealscript'
 author = 'Lyric'
 homepage = 'https://github.com/whtsky/catsup-theme-sealscript'
-pages = [
-    '404.html'
-]
 vars = {
     "description": "a blog",
     "github": "whtsky",
@@ -12,16 +9,6 @@ vars = {
             "name": "catsup",
             "url": "https://github.com/whtsky/catsup",
             "description": "Awesome!"
-        },
-        {
-            "name": "whtsky",
-            "url": "http://whouz.com",
-            "description": "I write catsup"
-        },
-        {
-            "name": "messense",
-            "url": "http://messense.me",
-            "description": "I also write catsup"
         }
     ]
 }
