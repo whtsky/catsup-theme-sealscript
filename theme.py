c80b935dddef59090aed551d2ab3b98ff6f409cf
whtsky <whtsky@me.com>
2013-08-18 17:50:10 +0800

    add post_per_page

--- theme.py
+++ theme.py
@@ -1,6 +1,7 @@
 name = 'sealscript'
 author = 'Lyric'
 homepage = 'https://github.com/whtsky/catsup-theme-sealscript'
+post_per_page = 3
 vars = {
     "description": "a blog",
     "github": "whtsky",
